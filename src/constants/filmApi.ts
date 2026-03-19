140a82c10044bf022401162ebd291e287ef806a3
Максим Беспалов <bespalov-maksim@list.ru>
2026-03-19 20:30:34 +0300

    update dist files

--- src/constants/filmApi.ts
+++
@@ -1,6 +0,0 @@
-export const API = {
-  FILMS_PER_PAGE: 50,
-  API_KEY: import.meta.env.VITE_API_KEY,
-  API_URL: 'https://api.poiskkino.dev/v1.4/movie',
-  //API_URL: 'http://localhost:3001/movies',
-}
--- src/types/store.ts
+++
@@ -1,3 +0,0 @@
-import type { FavoriteSlice } from "../store/favoriteSlice";
-
-export type Store = FavoriteSlice;
